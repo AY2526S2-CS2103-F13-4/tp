c6deab328f95e7f5a0aa06f0ffdb7f3fa1551cfb
bitxer <24569832+bitxer@users.noreply.github.com>
2026-03-22 22:24:28 +0800

    add findPersonDescriptor

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -3,15 +3,19 @@ package seedu.address.logic.commands;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.model.Model;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
 import seedu.address.model.person.TagsContainsTagPredicate;
+import seedu.address.model.tag.AbstractTag;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the
@@ -99,5 +103,97 @@ public class FindCommand extends Command {
                 .add("namePredicate", namePredicate)
                 .add("tagPredicate", tagPredicate)
                 .toString();
+    }
+
+    /**
+     * Stores the details to find the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class FindPersonDescriptor {
+        private Set<Name> name;
+        private Set<Phone> phone;
+        private Set<Email> email;
+        private Set<AbstractTag> tags;
+
+        public FindPersonDescriptor() {}
+
+
+        /**
+         * Copy constructor.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public FindPersonDescriptor(FindPersonDescriptor toCopy) {
+            setName(toCopy.name);
+            setPhone(toCopy.phone);
+            setEmail(toCopy.email);
+            setTags(toCopy.tags);
+        }
+
+        public void setName(Set<Name> name) {
+            this.name = name;
+        }
+
+        public Optional<Set<Name>> getName() {
+            return (name != null) ? Optional.of(Collections.unmodifiableSet(name)) : Optional.empty();
+        }
+
+        public void setPhone(Set<Phone> phone) {
+            this.phone = phone;
+        }
+
+        public Optional<Set<Phone>> getPhone() {
+            return (phone != null) ? Optional.of(Collections.unmodifiableSet(phone)) : Optional.empty();
+        }
+
+        public void setEmail(Set<Email> email) {
+            this.email = email;
+        }
+
+        public Optional<Set<Email>> getEmail() {
+            return (email != null) ? Optional.of(Collections.unmodifiableSet(email)) : Optional.empty();
+        }
+
+        /**
+         * Sets {@code tags} to this object's {@code tags}.
+         * A defensive copy of {@code tags} is used internally.
+         */
+        public <T extends AbstractTag> void setTags(Set<T> tags) {
+            this.tags = (tags != null) ? new HashSet<>(tags) : null;
+        }
+
+        /**
+         * Returns an unmodifiable tag set, which throws {@code UnsupportedOperationException}
+         * if modification is attempted.
+         * Returns {@code Optional#empty()} if {@code tags} is null.
+         */
+        public Optional<Set<AbstractTag>> getTags() {
+            return (tags != null) ? Optional.of(Collections.unmodifiableSet(tags)) : Optional.empty();
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other == this) {
+                return true;
+            }
+
+            // instanceof handles nulls
+            if (!(other instanceof FindPersonDescriptor otherFindPersonDescriptor)) {
+                return false;
+            }
+
+            return Objects.equals(name, otherFindPersonDescriptor.name)
+                    && Objects.equals(phone, otherFindPersonDescriptor.phone)
+                    && Objects.equals(email, otherFindPersonDescriptor.email)
+                    && Objects.equals(tags, otherFindPersonDescriptor.tags);
+        }
+        @Override
+        public String toString() {
+            return new ToStringBuilder(this)
+                    .add("name", name)
+                    .add("phone", phone)
+                    .add("email", email)
+                    .add("tags", tags)
+                    .toString();
+        }
     }
 }
